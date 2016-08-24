325fb388fb1d0db9a5222d6bef2a25416567ab1d
Bradley P. Allen <bradley.p.allen@gmail.com>
2016-08-24 14:16:17 -0700

    Added utility shell script to initialize Bitfusion AWS EC2 instance

---
+++ bitfusion-ubuntu-14-theano-aws-setup.sh
@@ -0,0 +1,9 @@
+git config --global user.name "Bradley P. Allen"
+git config --global user.email "bradley.p.allen@gmail.com"
+sudo apt-get -y install emacs
+sudo pip install pandas
+cd pynb
+git clone https://github.com/bradleypallen/fb15k-akbc.git
+cd fb15k-akbc/
+wget https://download.microsoft.com/download/8/7/0/8700516A-AB3D-4850-B4BB-805C515AECE1/FB15K-237.zip
+unzip FB15K-237.zip
